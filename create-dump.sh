55b47a0982529ff893497e44e7006af59b58baeb
Artur Tomusiak <artur.tomusiak@hannonhill.com>
2017-12-13 18:10:44 -0500

    Hiding output when creating a db dump

--- create-dump.sh
+++ create-dump.sh
@@ -7,5 +7,5 @@ cd $DATABASE_DUMP_PATH
 if [ -n "$PASS" ]; then
     PASSCMD="-p$PASS"
 fi
-mysqldump -u $USERNAME $PASSCMD -e --default-character-set=utf8 --add-drop-table $DBNAME > $1
+mysqldump -u $USERNAME $PASSCMD -e --default-character-set=utf8 --add-drop-table $DBNAME > $1 2> /dev/null
 echo 'Backup complete'
